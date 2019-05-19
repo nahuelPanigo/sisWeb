a2b385a755c73694b36e21fd40751c90597b9683
jimevillanueva <49253521+jimevillanueva@users.noreply.github.com>
2019-05-19 13:29:29 -0300

    con nuevas vistas

--- resources/views/estilos.php
+++ resources/views/estilos.php
@@ -2,3 +2,4 @@
 <link rel="stylesheet" type="text/css" href="/css/estilos.css">
 <link rel="stylesheet" type="text/css" href="/css/submenus.css">
 <link rel="shortcut icon" type="image/x-icon" href="/css/imagenes/hsh.png">
+asdasdas.
