4c1fcc14327969d52dbebcdb5b09f012b932c7b9
ramirolopescanadell <44657603+ramirolopescanadell@users.noreply.github.com>
2019-06-19 23:04:04 -0300

    cambios de header

--- resources/views/adminListarPropiedades.blade.php
+++ resources/views/adminListarPropiedades.blade.php
@@ -18,17 +18,15 @@
 		<div class="column">
 			<div class="card">
 				<h3>{{$propiedad->name}}</h3>
-				@Include('submenuPropiedad')
-				 @Include('eliminar')
-			
+			@Include('submenuPropiedad')
 			<div class="datos">
 				<p><span class="fas fa-map-marker-alt"></span> {{$propiedad -> locate}} </p>
 				<p><span class="fas fa-info-circle"></span> {{$propiedad -> description}} </p>
 			</div>
-
 			<img src="{{str_replace('public/', '/storage/', $propiedad->images()->first()->archiveName)}}" onclick="openModal();currentSlide(1)" class="hover-shadow cursor">
 		</div>
 	</div>
+	@Include('eliminar')
 	@endforeach
 	</div>
 	@include('galeriaDeFotos')
