8957baab4deee452f328d02515cf251a01084e84
ramirolopescanadell <44657603+ramirolopescanadell@users.noreply.github.com>
2019-07-05 20:05:28 -0300

    comprar y eliminar hotsale vistas

--- resources/views/eliminarHotsale.blade.php
+++ resources/views/eliminarHotsale.blade.php
@@ -1,9 +1,9 @@
 <div id="id01" class="modal">
   <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-  <form class="modal-content"  method="POST" action="{{route('hotsales.comprar')}}">
+  <form class="modal-content"  method="POST" action="{{route('hotsales.delete')}}">
   	{{ csrf_field()}}
     <div class="container">
-      <h1 style="text-align:center;"> Comprar Hot Sale </h1>
+      <h1 style="text-align:center;"> ¿Está seguro que quiere eliminar el hotsale? </h1>
 		</div>
       <div class="clearfix reservarBoton">
         <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancelar</button>
@@ -14,13 +14,13 @@
   </form>
 </div>.
 <script>
-
+// Get the modal
 function mostrarModal(id){
 				document.getElementById('hotsale_id').value= id;
 				document.getElementById('id01').style.display='block';
 			}
-// Get the modal
 var modal = document.getElementById('id01');
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -29,5 +29,3 @@ window.onclick = function(event) {
   }
 }
 </script>
-
-
