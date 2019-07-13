f9e778f5f19d42f88b4d4a3750f21a9185c1da35
ramirolopescanadell <44657603+ramirolopescanadell@users.noreply.github.com>
2019-07-13 16:36:07 -0300

    arreglos de estilos

--- resources/views/buscador.blade.php
+++ resources/views/buscador.blade.php
@@ -8,7 +8,7 @@
       <label> Localidad (opcional)</label>
       <input type="text" placeholder="Search.." name="search">
     </div>
-     <button type="submit" class="boton"><i class="fa fa-search"></i></button>
+     <button type="submit" class="boton_lupa"><i class="fa fa-search"></i></button>
     </form>
    
 </div>
--- resources/views/estilos.php
+++ resources/views/estilos.php
@@ -3,8 +3,6 @@
 <link rel="shortcut icon" type="image/x-icon" href="/css/imagenes/hsh.png">
  <meta name="viewport" content="width=device-width, initial-scale=1">
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  
-
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
